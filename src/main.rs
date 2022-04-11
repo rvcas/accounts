1dcc95d01214308494e1cd2dbb57536d27b53b89
rvcas <x@rvcas.dev>
2022-04-11 19:02:12 -0400

    feat: input and output logic
    * handle capturing a file path as args
    * read csv and serialize to struct
    * match on action
    * write accounts csv to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use std::{collections::HashMap, io};
+
+use accounts::{
+    cli::Cli,
+    model::{Account, Action, Transaction},
+};
+
+fn main() -> anyhow::Result<()> {
+    let args = Cli::default();
+
+    let raw_entries = std::fs::read(&args.input)?;
+
+    let mut transactions = HashMap::new();
+    let mut accounts = HashMap::new();
+
+    let mut rdr = csv::Reader::from_reader(raw_entries.as_slice());
+
+    for result in rdr.deserialize() {
+        let entry: Transaction = result?;
+
+        let transaction = transactions.entry(entry.id).or_insert(entry);
+
+        let account = accounts
+            .entry(entry.client_id)
+            .or_insert_with(|| Account::new(entry.client_id));
+
+        match entry.action {
+            Action::Deposit => {
+                account.available += entry.amount;
+                account.total += entry.amount;
+            }
+            Action::Withdraw => {
+                account.available -= entry.amount;
+                account.total -= entry.amount;
+            }
+        }
+    }
+
+    let mut wtr = csv::Writer::from_writer(io::stdout());
+
+    for account in accounts.values() {
+        wtr.serialize(account)?;
+    }
+
+    wtr.flush()?;
+
+    Ok(())
 }
---
+++ src/model.rs
@@ -0,0 +1,41 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Clone, Copy)]
+#[serde(rename_all = "lowercase")]
+pub enum Action {
+    Deposit,
+    Withdraw,
+}
+
+#[derive(Deserialize, Clone, Copy)]
+pub struct Transaction {
+    #[serde(rename = "type")]
+    pub action: Action,
+    #[serde(rename = "client")]
+    pub client_id: u16,
+    #[serde(rename = "tx")]
+    pub id: u32,
+    pub amount: f64,
+}
+
+#[derive(Serialize)]
+pub struct Account {
+    #[serde(rename = "client")]
+    pub id: u16,
+    pub available: f64,
+    pub held: f64,
+    pub total: f64,
+    pub locked: bool,
+}
+
+impl Account {
+    pub fn new(id: u16) -> Self {
+        Self {
+            id,
+            available: 0.0,
+            held: 0.0,
+            total: 0.0,
+            locked: false,
+        }
+    }
+}
